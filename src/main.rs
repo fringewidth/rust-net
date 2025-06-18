28a58672751b3bf86896396b4990f7486365cacc
Hrishik <116365046+fringewidth@users.noreply.github.com>
2025-06-18 08:38:04 +0530

    fix bug in matmul

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use rust_net::*;
 
 fn main() {
-    let mut mat: MatrixOwner<f32> = MatrixOwner::null(3, 4);
-    println!("A: \n{}", MatDisplay::to_string(&mat));
-    println!("Aᵀ:\n{}", MatDisplay::to_string(&mat.t()));
-    let matT = mat.t_mut();
-    let mut matResult = matmul(&mat, &matT);
-    matResult = matmul(&matResult, &MatrixOwner::eye(matResult.rows()));
+    let mut mat: MatrixOwner<f32> = MatrixOwner::normal(4, 2, 0., 1.);
+    let mut eye: MatrixOwner<f32> = MatrixOwner::eye(2);
 
-    println!("{}", matResult.to_string());
+    println!("Matrix A: \n{}", MatDisplay::to_string(&mat));
+    
+    println!("Matrix I: \n{}", MatDisplay::to_string(&eye));
+
+    println!("AI: \n{}", MatDisplay::to_string(&matmul(&mat, &eye)));
+
+    // println!("A: \n{}", MatDisplay::to_string(&mat));
+    // println!("Aᵀ:\n{}", MatDisplay::to_string(&mat.t()));
+    // let matT = mat.t_mut();
+    // let mut matResult = matmul(&mat, &matT);
+    // matResult = matmul(&matResult, &MatrixOwner::eye(matResult.rows()));
+
+    // println!("{}", matResult.to_string());
 }
--- src/matrix/algebra.rs
+++ src/matrix/algebra.rs
@@ -4,7 +4,7 @@ use std::ops::{Add, AddAssign, Mul};
 
 pub fn matmul<T, A, B>(matA: &A, matB: &B) -> MatrixOwner<T>
 where
-    T: Copy + Add<Output = T> + Mul<Output = T> + Default + One,
+    T: Copy + Add<Output = T> + Mul<Output = T> + std::ops::AddAssign + Default + One,
     for<'a, 'b> &'a T: Mul<&'b T, Output = T>,
     A: Matrix<T>,
     B: Matrix<T>,
@@ -22,7 +22,7 @@ where
     for i in 0..matA.rows() {
         for j in 0..matB.cols() {
             for k in 0..matA.cols() {
-                matC[(i, j)] = matA.at((i, k)) * matB.at((k, j));
+                matC[(i, j)] += matA.at((i, k)) * matB.at((k, j));
             }
         }
     }
